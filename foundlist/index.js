0bda913988913945a02a04cf20cd30f1b5ee52a1
Zachary Allen <ZacharyAllen654321@gmail.com>
2021-10-09 14:55:28 -0600

    added pseudocode

---
+++ foundlist/index.js
@@ -0,0 +1,44 @@
+const sampleData = [
+  {
+    imageUrl:
+      "https://images.unsplash.com/photo-1521774971864-62e842046145?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1169&q=80",
+    title: "Blue Sneakers",
+    description:
+      "Found these sneakers outside of the wilk. They are blue and I think they're adidas",
+  },
+  {
+    imageUrl:
+      "https://images.unsplash.com/photo-1521774971864-62e842046145?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1169&q=80",
+    title: "Blue Sneakers",
+    description:
+      "Found these sneakers outside of the wilk. They are blue and I think they're adidas",
+  },
+  {
+    imageUrl:
+      "https://images.unsplash.com/photo-1521774971864-62e842046145?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1169&q=80",
+    title: "Blue Sneakers",
+    description:
+      "Found these sneakers outside of the wilk. They are blue and I think they're adidas",
+  },
+];
+
+// convert a card to a template
+// grab the template and box by id.
+
+const cards = sampleData.map((item) => {
+  return; // item converted into a card
+});
+
+// grab the box and clear it
+
+// append(card)
+
+//get from the firebase api the link
+
+// format it
+
+// convert to cards
+
+// append
+const clampedCards = (document.getElementById("clampedCards").innerHtml = null);
+const clampedCards = document.getElementById("clampedCards").append(cards);
