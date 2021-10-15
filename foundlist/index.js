df7bfeb61f9c3508f47ee9df7d0a3c71919edf06
usofallen <dballen@byu.edu>
2021-10-15 15:03:02 -0600

    Connected to Firebase API, edited homepage a bit

--- foundlist/index.js
+++ foundlist/index.js
@@ -1,44 +1,78 @@
-const sampleData = [
-  {
-    imageUrl:
-      "https://images.unsplash.com/photo-1521774971864-62e842046145?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1169&q=80",
-    title: "Blue Sneakers",
-    description:
-      "Found these sneakers outside of the wilk. They are blue and I think they're adidas",
-  },
-  {
-    imageUrl:
-      "https://images.unsplash.com/photo-1521774971864-62e842046145?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1169&q=80",
-    title: "Blue Sneakers",
-    description:
-      "Found these sneakers outside of the wilk. They are blue and I think they're adidas",
-  },
-  {
-    imageUrl:
-      "https://images.unsplash.com/photo-1521774971864-62e842046145?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1169&q=80",
-    title: "Blue Sneakers",
-    description:
-      "Found these sneakers outside of the wilk. They are blue and I think they're adidas",
-  },
-];
+// const sampleData = [
+//   {
+//     imageUrl:
+//       "https://images.unsplash.com/photo-1521774971864-62e842046145?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1169&q=80",
+//     title: "Blue Sneakers",
+//     description:
+//       "Found these sneakers outside of the wilk. They are blue and I think they're adidas",
+//   },
+//   {
+//     imageUrl:
+//       "https://images.unsplash.com/photo-1521774971864-62e842046145?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1169&q=80",
+//     title: "Blue Sneakers",
+//     description:
+//       "Found these sneakers outside of the wilk. They are blue and I think they're adidas",
+//   },
+//   {
+//     imageUrl:
+//       "https://images.unsplash.com/photo-1521774971864-62e842046145?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1169&q=80",
+//     title: "Blue Sneakers",
+//     description:
+//       "Found these sneakers outside of the wilk. They are blue and I think they're adidas",
+//   },
+// ];
 
-// convert a card to a template
-// grab the template and box by id.
 
-const cards = sampleData.map((item) => {
-  return; // item converted into a card
-});
+  let temp = function(url) {
+    fetch(url)
+      .then(function(response) {
+        return response.json();
+      }).then(function(json) {
+          console.log(json);
+          let remainingHTML = "";
 
-// grab the box and clear it
+          remainingHTML += "<div class='card'><img src='" + json.fields.imgURL.stringValue
+            + "' class='card-img-top' alt='...' style='height: 150px; object-fit: cover; width: 100%'/>"
+          remainingHTML += "<div class='card-body'><h5 class='card-title'>" + json.fields.name.stringValue + "</h5>"
+          remainingHTML += "<p class='card-text'>" + json.fields.description.stringValue + "</p>"
+          remainingHTML += "<a href='#' class='btn btn-primary'>Claim</a></div></div>"
 
-// append(card)
+          let cardTemplate = document.createElement('div');
+          cardTemplate.classList.add("p-2");
+          cardTemplate.classList.add("col-12");
+          cardTemplate.classList.add("col-sm-6");
 
-//get from the firebase api the link
+          cardTemplate.innerHTML = remainingHTML;
+          document.getElementById("cardTemplate").appendChild(cardTemplate);
+            console.log(remainingHTML);
+      });
+  }
+  
+  temp("https://firestore.googleapis.com/v1/projects/lost-and-found-24934/databases/(default)/documents/lost/CaKMeHk4JUUiBzP3RGt0/entries/entry1");
+  temp("https://firestore.googleapis.com/v1/projects/lost-and-found-24934/databases/(default)/documents/lost/CaKMeHk4JUUiBzP3RGt0/entries/entry2");
+  temp("https://firestore.googleapis.com/v1/projects/lost-and-found-24934/databases/(default)/documents/lost/CaKMeHk4JUUiBzP3RGt0/entries/entry3");
+  temp("https://firestore.googleapis.com/v1/projects/lost-and-found-24934/databases/(default)/documents/lost/CaKMeHk4JUUiBzP3RGt0/entries/entry4");
+        
+        
 
-// format it
 
-// convert to cards
 
-// append
-const clampedCards = (document.getElementById("clampedCards").innerHtml = null);
-const clampedCards = document.getElementById("clampedCards").append(cards);
+
+// // convert a card to a template
+// // grab the template and box by id.
+
+
+
+// // grab the box and clear it
+
+// // append(card)
+
+// //get from the firebase api the link
+
+// // format it
+
+// // convert to cards
+
+// // append
+// const clampedCards = (document.getElementById("clampedCards").innerHtml = null);
+// const clampedCards = document.getElementById("clampedCards").append(cards);
